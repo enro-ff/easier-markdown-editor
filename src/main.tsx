f35af46a8c480791c099d1211e02342c3977b296
ffxd <2124563502@qq.com>
2026-01-18 16:14:18 +0800

    base-page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import zhCN from "antd/locale/zh_CN";
+import "dayjs/locale/zh-cn";
+import { ConfigProvider } from "antd";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </StrictMode>,
-)
+);
